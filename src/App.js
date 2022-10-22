a0f8197f15b066ac59cf07a4938671d84e80c1af
Adrian <adrian.e.rosales@gmail.com>
2022-10-22 01:20:39 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./Containers/Home";
+import Navigation from "./Components/Navigation";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataLayer>
+      <Router>
+        <Navigation />
+        <Switch>
+          {/* <Route path="/game">
+          <Game />
+        </Route> */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </DataLayer>
   );
-}
+};
 
 export default App;
---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,7 @@
+
+export const Home = () => {
+    return (
+        <h1>Hello To Infinite Wordle</h1>
+    )
+}
+
